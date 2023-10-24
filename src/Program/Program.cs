b117825a84ddee03d6061ceff8762fdb230f043c
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2023-10-24 17:34:50 -0300

    Ejercicio 2 hecho y ejercicio 1 editado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,22 +7,46 @@ namespace CompAndDel
     class Program
     {
         static void Main(string[] args)
-        //Ejercicio 1
-        {
-            PictureProvider provider = new PictureProvider();
-            IPicture image = provider.GetPicture(@"luke.jpg");
+        // EJERCICIO 1
+        // {
+        //     PictureProvider provider = new PictureProvider();
+        //     IPicture image = provider.GetPicture(@"luke.jpg");
 
-            IPipe pipenull = new PipeNull ();
-            image = pipenull.Send(image);
-            IFilter blur = new FilterNegative();
+        //     IPipe pipenull = new PipeNull ();
+        //     image = pipenull.Send(image);
+        //     IFilter blur = new FilterNegative();
             
-            IPipe pipeserial1 = new PipeSerial(blur, pipenull);
-            image = pipeserial1.Send(image);
-            IFilter negative = new FilterGreyscale();
+        //     IPipe pipeserial1 = new PipeSerial(blur, pipenull);
+        //     image = pipeserial1.Send(image);
+        //     IFilter negative = new FilterGreyscale();
 
-            IPipe pipeserial2 = new PipeSerial(negative, pipeserial1);
+        //     IPipe pipeserial2 = new PipeSerial(negative, pipeserial1);
+        //     image = pipeserial1.Send(image);
 
-            provider.SavePicture(image, @"luke.jpg");
-        }
+        //     provider.SavePicture(image, @"luke.jpg");
+        // }
+
+        // EJERCICIO 2
+        // {
+        //     PictureProvider provider = new PictureProvider();
+        //     IPicture image = provider.GetPicture(@"luke.jpg");
+
+        //     IPipe pipenull = new PipeNull ();
+        //     image = pipenull.Send(image);
+        //     IFilter blur = new FilterBlurConvolution();
+            
+        //     IPipe pipeserial1 = new PipeSerial(blur, pipenull);
+        //     image = pipeserial1.Send(image);
+        //     IFilter negative = new FilterNegative();
+
+        //     IPipe pipeserial2 = new PipeSerial(negative, pipeserial1);
+        //     image = pipeserial1.Send(image);
+
+        //     PictureProvider p = new PictureProvider();
+        //     p.SavePicture(image, "LukeEditado.jpg");
+
+
+
+        // }
     }
 }
