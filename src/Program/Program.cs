bffe49b16b048d9ecd73c694bd80622d0fec58ec
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2023-10-24 17:16:06 -0300

    Ejercicio 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,7 +7,22 @@ namespace CompAndDel
     class Program
     {
         static void Main(string[] args)
+        //Ejercicio 1
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture image = provider.GetPicture(@"luke.jpg");
+
+            IPipe pipenull = new PipeNull ();
+            image = pipenull.Send(image);
+            IFilter blur = new FilterNegative();
+            
+            IPipe pipeserial1 = new PipeSerial(blur, pipenull);
+            image = pipeserial1.Send(image);
+            IFilter negative = new FilterGreyscale();
+
+            IPipe pipeserial2 = new PipeSerial(negative, pipeserial1);
+
+            provider.SavePicture(image, @"luke.jpg");
         }
     }
 }
